6e8b6d90b88ab43de339c534a0e390a547a00917
caty <caterina.deiacobis@gmail.com>
2018-11-03 15:25:28 +0100

    inserito comando per Heroku

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ var app = express();
 
 var path = require('path');
 
+const PORT = process.env.PORT ||3000; //serve per far funzionare heroku
+
 // connessione al database
 require('./config/db.js');
 
@@ -15,6 +17,6 @@ var livereload = require('livereload');
 var server = livereload.createServer();
 server.watch(path.join(__dirname, "..", "public"));
 
-app.listen(3000, function () {
+app.listen(PORT, function () {
     console.log("http://localhost:3000")
 })
